6be167054782e97c4b9950f89006db72b3d1403d
Alex <alex.iasoni@isen.yncrea.fr>
2020-02-11 15:08:27 +0100

    V2

---
+++ app/src/main/java/fr/isen/iasoni/studentapplication/View/Login.kt
@@ -0,0 +1,13 @@
+package fr.isen.iasoni.studentapplication.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import fr.isen.iasoni.studentapplication.R
+
+class Login : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/fr/isen/iasoni/studentapplication/View/Profil.kt
@@ -0,0 +1,13 @@
+package fr.isen.iasoni.studentapplication.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import fr.isen.iasoni.studentapplication.R
+
+class Profil : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profil)
+    }
+}
