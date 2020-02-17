a541415f170aeeecdc3322c5c357d92710b55f84
IlaneLopez <44532151+IlaneLopez@users.noreply.github.com>
2020-02-17 14:36:22 +0100

    Update ProfilActivity.kt

--- app/src/main/java/fr/isen/iasoni/studentapplication/View/ProfilActivity.kt
+++ app/src/main/java/fr/isen/iasoni/studentapplication/View/ProfilActivity.kt
@@ -2,12 +2,31 @@ package fr.isen.iasoni.studentapplication.View
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import fr.isen.iasoni.studentapplication.R
+import kotlinx.android.synthetic.main.activity_profil.*
 
 class ProfilActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profil)
+
+        navigation_view.setOnNavigationItemSelectedListener {item ->
+
+            var message = ""
+            when(item.itemId){
+                R.id.action_home -> message = "Home"
+                R.id.action_notification -> message = "Notification"
+                R.id.action_profil -> message = "Profil"
+                R.id.action_events -> message = "Events"
+                R.id.action_swipe -> message = "Swipe"
+
+
+            }
+            Toast.makeText(this@ProfilActivity, "$message clicked!", Toast.LENGTH_SHORT).show()
+
+            return@setOnNavigationItemSelectedListener true
+        }
     }
 }
