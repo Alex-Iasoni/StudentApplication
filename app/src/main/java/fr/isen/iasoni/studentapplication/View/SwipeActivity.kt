8fa74acbec8477456551597bad0fa1515b4b8801
IlaneLopez <ilane.lopez@outlook.fr>
2020-02-21 09:18:23 +0100

    Logo red + reduction dimension alaiz.jpg + halfpotatoes

---
+++ app/src/main/java/fr/isen/iasoni/studentapplication/View/SwipeActivity.kt
@@ -0,0 +1,12 @@
+package fr.isen.iasoni.studentapplication.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SwipeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_swipe)
+    }
+}
