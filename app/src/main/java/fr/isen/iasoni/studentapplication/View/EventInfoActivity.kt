22e4a13f8370ad45727f79b967af19fa241e4122
Thomas BERTON <thomas.berton@isen.yncrea.fr>
2020-03-04 15:31:53 +0100

    edit event

--- app/src/main/java/fr/isen/iasoni/studentapplication/View/EventInfoActivity.kt
+++ app/src/main/java/fr/isen/iasoni/studentapplication/View/EventInfoActivity.kt
@@ -1,4 +1,14 @@
 package fr.isen.iasoni.studentapplication.View
 
-class EventInfoActivity {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import fr.isen.iasoni.studentapplication.R
+
+class EventInfoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_event_info)
+
+    }
 }
