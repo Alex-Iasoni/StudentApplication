2ec2194326b4fe0a1702d2d5ba73c93c0a4d04e5
IlaneLopez <ilane.lopez@outlook.fr>
2020-03-08 13:24:07 +0100

    Merge branch 'master' of https://github.com/Alex-Iasoni/StudentApplication
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/fr/isen/iasoni/studentapplication/View/CreateEventActivity.kt
+++ app/src/main/java/fr/isen/iasoni/studentapplication/View/CreateEventActivity.kt
@@ -222,7 +222,8 @@ open class CreateEventActivity : AppCompatActivity(), TimePickerDialog.OnTimeSet
             val uid = FirebaseAuth.getInstance().uid ?: ""
             var eventController = EventController()
             var a = 100
-            eventController.createEvent(eventTitle.text.toString(),uid,eventPlace.text.toString(), "", ville.toString(), school.toString(), arrayMusic, date_event_input.text.toString(), date_event_input_2.text.toString(), eventDescription.text.toString(), "false",  a.toString())
+            var etudiant = "false"
+            eventController.createEvent(eventTitle.text.toString(),uid,eventPlace.text.toString(), "", ville.toString(), school.toString(), arrayMusic, date_event_input.text.toString(), date_event_input_2.text.toString(), eventDescription.text.toString(), etudiant.toString(),  a.toString())
 
             eventController.FindIdEvent(eventTitle.text.toString(), uid){
                 val foo = Intent(this, EventInfoActivity::class.java)
