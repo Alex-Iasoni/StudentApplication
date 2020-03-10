f0793528404bec20662e59b5f95d4b077a0a1c0d
IlaneLopez <ilane.lopez@outlook.fr>
2020-03-10 20:07:43 +0100

    Merge branch 'master' of https://github.com/Alex-Iasoni/StudentApplication
    # Conflicts:
    #	app/src/main/java/fr/isen/iasoni/studentapplication/Controller/EventController.kt

--- app/src/main/java/fr/isen/iasoni/studentapplication/Controller/EventController.kt
+++ app/src/main/java/fr/isen/iasoni/studentapplication/Controller/EventController.kt
@@ -439,12 +439,12 @@ subs.NumberSubscribeUser(id_subscribe_event){
                                 interest.add(false)
                             }
 
+                            callback.invoke(interest)
 
                         }
                     }
                 }
 
-                callback.invoke(interest)
             }
 
         }
